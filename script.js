14caae2216ba4739a3e26771bfa0c2aad0e3696e
sumayabaraako <sumaya_baraako2001@hotmail.com>
2026-06-13 22:54:53 +0200

    add custom theme functionality

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const themeSelect = document.getElementById("themeSelect");
 
+const newThemeInput = document.getElementById("newThemeInput");
+const addThemeBtn = document.getElementById("addThemeBtn");
+
 let themes = [
   "Crowth",
   "Work",
@@ -17,6 +20,27 @@ function loadThemes() {
 
     themeSelect.appendChild(option);
   });
+
+  
+  addThemeBtn.addEventListener("click", () => {
+
+  const newTheme = newThemeInput.value.trim();
+
+  if (newTheme === "") {
+    return;
+  }
+
+  themes.push(newTheme);
+
+  loadThemes();
+
+  themeSelect.value = newTheme;
+
+  newThemeInput.value = "";
+
+});
+
+
 }
 
 loadThemes();
